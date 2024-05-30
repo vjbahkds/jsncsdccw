2b5da145c162e3893513aa339355046a82345a5a
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-05-30 17:03:19 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -9,7 +9,7 @@ RandString() {
 
 # Debian12+
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
-sudo apt -qqy install wget nload procps icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget nload procps icu-devtools netcat-traditional >/dev/null 2>&1
+sudo apt -qqy install wget procps icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps icu-devtools netcat-traditional >/dev/null 2>&1
 
 
 cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
