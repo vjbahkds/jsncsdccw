8b2bdf32d86695c9b14f644ed90469edeb61e23b
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-07 13:15:41 +0800

    Update k2.sh

--- k2.sh
+++ k2.sh
@@ -8,7 +8,7 @@ mName="${4:-./bash}"
 function task(){
   if [ -n "$mName" ]; then
     for mPid in `ps -ef |grep "${mName} " |grep -v 'grep' |head -n1 |awk '{print $2}'`; do
-      [ -n "$mPid" ] && echo "kill: $mPid && kill -9 "$mPid" >/dev/null 2>&1
+      [ -n "$mPid" ] && echo "kill: $mPid" && kill -9 "$mPid" >/dev/null 2>&1
     done
   fi
   [ -f "${work}/appsettings.json" ] || return 0;
