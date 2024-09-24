1b333605cac12f72e1e481352c58fc02b9a6d075
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-24 16:06:38 +0800

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -2,6 +2,7 @@
 
 token="${1:-}"
 version="${2:-}"
+hugepage="1024"
 
 [ -n "$token" ] || exit 1
 [ -n "$version" ] || version=`wget -qO- https://api.github.com/repos/apool-io/apoolminer/releases/latest |grep '"tag_name":' |cut -d'"' -f4`
@@ -9,6 +10,7 @@ version="${2:-}"
 
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
 sudo apt -qqy install wget procps lsof icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps lsof icu-devtools netcat-traditional >/dev/null 2>&1
+sudo sysctl -w vm.nr_hugepages=$hugepage >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$hugepage >/dev/null 2>&1
 
 tmp=`mktemp -d`;
 wget -qO- "https://github.com/apool-io/apoolminer/releases/download/${version}/apoolminer_linux_${version}.tar" |tar -zx -C "$tmp"
